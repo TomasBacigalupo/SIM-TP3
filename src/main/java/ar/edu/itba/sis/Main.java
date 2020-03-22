3dad2870ad34f170484bfce3f9087646d6aa6e51
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-03-22 18:54:50 -0300

    mas parametros

--- src/main/java/ar/edu/itba/sis/Main.java
+++ src/main/java/ar/edu/itba/sis/Main.java
@@ -4,24 +4,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class Main
 {
     public static void main( String[] args ) throws FileNotFoundException {
         double L = 0;
+        int N = 0;
         File config = new File("./config.txt");
         Scanner scn = new Scanner(config);
         while (scn.hasNextLine()){
             String line = scn.nextLine();
             String[] params = line.split(" ");
-            if (params[0].compareTo("L=") == 0){
+            String variable = params[0];
+            if (variable.compareTo("L=") == 0){
                 L = new Double(params[1]);
+            }else if (variable.compareTo("N=")== 0){
+                N = new Integer(params[1]);
             }
         }
 
-        System.out.println( L );
+        System.out.println( "(L="+L+", "+"N="+N+")");
     }
 }
