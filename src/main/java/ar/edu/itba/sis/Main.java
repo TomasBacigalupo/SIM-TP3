cc29d65893488da40151f95c3ae7e643173f6a47
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-03-23 19:07:46 -0300

    bien hecho el archivo de configuracion

--- src/main/java/ar/edu/itba/sis/Main.java
+++ src/main/java/ar/edu/itba/sis/Main.java
@@ -1,27 +1,27 @@
 package ar.edu.itba.sis;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main
 {
-    public static void main( String[] args ) throws FileNotFoundException {
+    public static void main( String[] args ) throws IOException {
+
         double L = 0;
         int N = 0;
-        File config = new File("./config.txt");
-        Scanner scn = new Scanner(config);
-        while (scn.hasNextLine()){
-            String line = scn.nextLine();
-            String[] params = line.split(" ");
-            String variable = params[0];
-            if (variable.compareTo("L=") == 0){
-                L = new Double(params[1]);
-            }else if (variable.compareTo("N=")== 0){
-                N = new Integer(params[1]);
-            }
-        }
+
+        InputStream input = new FileInputStream("src/config.properties");
+        Properties prop = new Properties();
+        prop.load(input);
+        N = new Integer(prop.getProperty("sim.N"));
+        L = new Integer(prop.getProperty("dim.L"));
+
 
         System.out.println( "(L="+L+", "+"N="+N+")");
+
+
+
+
     }
 }
