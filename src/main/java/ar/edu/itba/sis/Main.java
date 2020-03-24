8c1df94bd425e0563ad96ab3158277e8f2e8bc36
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-03-24 17:10:15 -0300

    Emprolijando

--- src/main/java/ar/edu/itba/sis/Main.java
+++ src/main/java/ar/edu/itba/sis/Main.java
@@ -11,37 +11,28 @@ public class Main
 {
     public static void main( String[] args ) throws IOException {
 
-        double L = 0;
+        double L;
+        int N;
 
-        int N = 0;
-
+        //set variables from config.properties
         InputStream input = new FileInputStream("src/resources/config.properties");
         Properties prop = new Properties();
         prop.load(input);
         N = new Integer(prop.getProperty("sim.N"));
         L = new Double(prop.getProperty("sim.L"));
 
-        System.out.println( "(L="+L+", "+"N="+N+")");    		
         /*
-
         L = 0.5m
-
         N partículas chicas de R1 = 0.005m m1 = 0.1g
         1 partícula  grande de R2 = 0.05m  m2 = 100g
-
         Las posiciones de todas las partículas deben ser al azar con distribución uniforme dentro del dominio.
         Las partículas pequeñas deben tener velocidades con una distribución uniforme en el rango: |v| < 0.1 m/s.
-
         La partícula grande debe tener velocidad inicial v2 = 0 y su posición inicial en x2=L/2, y2=L/2.
-
         */
+
         Board board = new Board(L,N);
-
-
-        FileWriter fw = new FileWriter("prueba2.txt");
-        String ovito = board.toOvito();
-
-        fw.write(ovito);
+        FileWriter fw = new FileWriter("ovito.txt");
+        fw.write(board.toOvito());
         fw.close();
 
 
